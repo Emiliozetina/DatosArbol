ac83921d0fe6feda7dcc05168472876c6b283c21
Emilio Zetina <emilio.zetina@anahuac.mx>
2025-11-07 12:20:16 -0600

    First

---
+++ README.md
@@ -0,0 +1,11 @@
+Tarea de Algoritmos 
+
+Emilio Zetina Sánchez 
+Alfredo Vieto Solis 
+
+00556615
+00558680
+
+Ingenieria en TI Anahuac Mayab 
+
+Estructura de Datos y Algoritmos 
---
+++ src/Nodo.java
@@ -0,0 +1,9 @@
+class Nodo {
+    String valor;
+    Nodo izquierdo, derecho;
+
+    Nodo(String valor) {
+        this.valor = valor;
+        izquierdo = derecho = null;
+    }
+}
